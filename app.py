a0b26a7edc432f70c69075f26c4ca0790b80ec1c
Satyam-Singh-x <singhsatyam.0912@gmail.com>
2025-07-25 14:36:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 def transform_text(text):
     text = text.lower()
